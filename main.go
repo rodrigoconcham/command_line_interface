907d7d114a36e9b3f48ab301410274d0fd5a3343
Rodrigo C. <rodrigoconcham@gmail.com>
2025-11-27 17:01:23 -0300

    log

--- main.go
+++ main.go
@@ -3,29 +3,49 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 var (
-	url string
+	url      string
+	logFile  string
+	logger   *log.Logger
+	interval time.Duration
 )
 
 func main() {
 	flag.StringVar(&url, "url", "", "URL to check")
-
+	flag.StringVar(&logFile, "logfile", "healthcheck.log", "file to log output to")
+	flag.DurationVar(&interval, "interval", 2*time.Second, "Interval betwween healthchecks")
 	flag.Parse()
 
-	if url != "" {
+	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating log file %s: %v", logFile, err)
+		log.Fatal(err)
+	}
+	defer file.Close()
+	logger = log.New(io.MultiWriter(file, os.Stdout), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
 		checkURL(url)
 	}
+
 }
 
 func checkURL(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting url %s: %v", url, err)
+		logger.Printf("Error getting url %s: %v\n", url, err)
 		return
 	}
-	fmt.Printf("Checked %s, Status: %d", url, resp.StatusCode)
+	logger.Printf("Checked %s, Status: %d\n", url, resp.StatusCode)
 }
